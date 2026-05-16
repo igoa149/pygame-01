078b4f223398e49dae35182186ebe6712124d19f
igoa149 <igoa149@gmail.com>
2026-05-16 18:42:23 +0900

    사각형 rgb 추가

--- game.py
+++ game.py
@@ -8,4 +8,9 @@ while True:
         if event.type == 256:
             break
     if event.type == 256:
-        break   
+        break  
+    
+    pygame.draw.rect(screen, (255, 0, 0), (100, 100, 50, 50))
+    pygame.draw.rect(screen, (0, 255, 0), (150, 100, 50, 50))
+    pygame.draw.rect(screen, (0, 0, 255), (200, 100, 50, 50))
+    pygame.display.flip()
